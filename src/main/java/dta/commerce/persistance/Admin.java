eee342a1e3421cb25cb75d63e9d31b2bb287c869
NathanGUILLOTEAU <guilloteau.nathan@gmail.com>
2015-08-06 09:22:58 +0200

    #1 - RDV : Version finale

--- src/main/java/dta/commerce/persistance/Admin.java
+++ src/main/java/dta/commerce/persistance/Admin.java
@@ -4,7 +4,7 @@ public class Admin extends Personne {
 	
 	/** 
 	 * Constructeurs
-	 */
+	 */ 
 	public Admin(Integer id, String nom, String prenom, String login,
 			String password) {
 		super(id, nom, prenom, login, password);
