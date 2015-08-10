1da99584936d22c6c2f5e98a7936021711f875b0
Simon Offredo <offredo.simon@gmail.com>
2015-08-10 11:45:12 +0200

    #2 ajout du put pour update produit

--- src/main/java/dta/commerce/rest/ProduitJAX.java
+++ src/main/java/dta/commerce/rest/ProduitJAX.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -52,6 +53,18 @@ public class ProduitJAX {
 	public Response deleteProduit(@PathParam("id") Integer idProduit) {
 		try {
 			produitEjb.deleteProduit(idProduit);
+		} catch (Exception e) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		return Response.status(Response.Status.OK).build();
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateProduit(Produit produit) {
+		try {
+			produitEjb.updateProduit(produit);
 		} catch (Exception e) {
 			return Response.status(Response.Status.BAD_REQUEST).build();
 		}
