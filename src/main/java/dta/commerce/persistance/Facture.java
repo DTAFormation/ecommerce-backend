5c54dc4eb469d4700434c74a7e11efa7659e0d8b
Frédéric Machaux <frederic.machaux@gmail.com>
2015-08-26 12:19:42 +0200

    travail sur les factures et les commandes

--- src/main/java/dta/commerce/persistance/Facture.java
+++ src/main/java/dta/commerce/persistance/Facture.java
@@ -29,12 +29,15 @@ public class Facture {
 	private String modepaiement;
 	
 	@ManyToOne
+	@JsonManagedReference
 	private Adresse adresselivraison;
 	
 	@ManyToOne
+	@JsonManagedReference
 	private Adresse adressefacturation;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
+	@JsonManagedReference
 	private CommandeClient commandeClient;
 
 	
